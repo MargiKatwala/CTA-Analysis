4082af420b3d9129652e54b456e7422228062e18
Margi Katwala <35939575+MargiKatwala@users.noreply.github.com>
2018-05-17 12:00:33 -0500

    Delete jjj.sql

--- jjj.sql
+++
@@ -1,7 +0,0 @@
-﻿Select Stations.Name from Stations
-                WHERE Stations.Name LIKE '%Clark%'
-                Group by Stations.Name Order by Stations.Name asc
-UPDATE Stops
-   SET ADA = 0
-   WHERE Stops.Name = 'Clark/Lake (Inner Loop)'
- 
